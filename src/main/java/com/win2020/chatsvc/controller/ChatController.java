17ee68d4e260477af72d8f97bbafe0ddf9197fb0
bransonperkins <bransonperkins@gmail.com>
2020-08-20 09:06:08 -0400

    set cross origin

--- src/main/java/com/win2020/chatsvc/controller/ChatController.java
+++ src/main/java/com/win2020/chatsvc/controller/ChatController.java
@@ -10,10 +10,12 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
+@CrossOrigin(value = "https://gaming-lounge-frontend.herokuapp.com/chat")
 public class ChatController {
 
     @Autowired private SimpMessagingTemplate messagingTemplate;
